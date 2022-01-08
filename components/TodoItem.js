5a4a252b000eff47245f56e8e8fe58b99fa93669
Pedro Antônio <gnx@tuta.io>
2022-01-08 13:33:51 -0300

    feat: add delete icon

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TodoItem ({ item, pressHandler }) {
     return (
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+        <TouchableOpacity>
+            <View style={styles.item}>
+                <Text>{item.text}</Text>
+                <TouchableOpacity onPress={() => pressHandler(item.key)}>
+                    <MaterialIcons name='delete' size={24} color='#333'/>
+                </TouchableOpacity>
+            </View>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     item: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         padding: 16,
         marginTop: 16,
         borderColor: '#bbb',
